a6286d8b323341e3a6cb5a9afcdd4825252499b6
ecorpcompanygrouptuneziki <tunezikiofficial@gmail.com>
2024-01-15 15:03:00 +0300

    ANDROID LOGIN PAGE

--- Advance-Native/START/AppStart.js
+++ Advance-Native/START/AppStart.js
@@ -1,11 +1,12 @@
 import { ANDROIDHOMEPAGE } from "../ENVIRONMENT/ANDROID/PAGES/HOMEPAGE/HomePage.js"
+import { ANDROIDLOGINPAGE } from "../ENVIRONMENT/ANDROID/PAGES/LOGINPAGE/LoginPage.js";
 
 const APPSTART=(DIV,ADVANCE)=>{
 
  
 if (localStorage.getItem('Device')==='Android') {
     
-    ANDROIDHOMEPAGE(DIV,ADVANCE);
+    ANDROIDLOGINPAGE(DIV,ADVANCE);
 
 } else {
     
