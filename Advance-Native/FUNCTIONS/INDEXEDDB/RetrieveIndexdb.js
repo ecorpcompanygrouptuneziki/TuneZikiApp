f1df1730356ea6a4eec85a96a659eaead5c577f3
ecorpcompanygrouptuneziki <tunezikiofficial@gmail.com>
2024-01-16 10:20:11 +0300

    ANDROID AUTOLOGIN PAGE

--- Advance-Native/FUNCTIONS/INDEXEDDB/RetrieveIndexdb.js
+++ Advance-Native/FUNCTIONS/INDEXEDDB/RetrieveIndexdb.js
@@ -2,12 +2,33 @@ const RETRIEVEINDEXEDDB = (DATABASE, DATABASETABLE, key) => {
     return new Promise((resolve, reject) => {
         const dbName = DATABASE;
 
-        const openRequest = indexedDB.open(dbName);
+        const openRequest = indexedDB.open(dbName, 1);
+
+        openRequest.onupgradeneeded = (event) => {
+            const db = event.target.result;
+
+            // Create object store if it doesn't exist
+            if (!db.objectStoreNames.contains(DATABASETABLE)) {
+                const objectStore = db.createObjectStore(DATABASETABLE, { keyPath: 'id', autoIncrement: true });
+
+                // Put demo data into the object store
+                const demoData = [
+                    { data: 'Demo 1' },
+                    { data: 'Demo 2' },
+                    // Add more demo data as needed
+                ];
+
+                demoData.forEach((item) => {
+                    objectStore.add(item);
+                });
+            }
+        };
 
         openRequest.onsuccess = (event) => {
             const db = event.target.result;
 
-            const transaction = db.transaction(DATABASETABLE, 'readonly');
+            const transaction = db.transaction([DATABASETABLE], 'readonly');
+
             const objectStore = transaction.objectStore(DATABASETABLE);
 
             const getRequest = objectStore.get(key);
