10c3cdfe3fa8187e3b93ac5131c5153007632cd2
ecorpcompanygrouptuneziki <tunezikiofficial@gmail.com>
2024-01-17 12:43:50 +0300

    ANDROID NOTIFICATIONS TEST

--- Advance-Native/PLUGINS/NOTIFICATIONS/Notifications.js
+++ Advance-Native/PLUGINS/NOTIFICATIONS/Notifications.js
@@ -20,5 +20,5 @@ const NOTIFICATIONS = (MESSAGE) => {
     }
   };
   
-  export { NOTIFICATIONS };
+  export {  };
   
--- Advance-Native/START/AppStart.js
+++ Advance-Native/START/AppStart.js
@@ -4,6 +4,8 @@ const APPSTART=(DIV,ADVANCE)=>{
 
 if (localStorage.getItem('Device')==='Android') {
     
+    ADVANCE.NOTIFICATIONS('Are they Working')
+
     ANDROIDLOGINPAGE(DIV,ADVANCE)
 
 } else {
