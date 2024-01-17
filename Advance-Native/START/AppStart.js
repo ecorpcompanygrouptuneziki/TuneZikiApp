3f9ad57072ed9479a8fdf90a3f180be21e9f5c0f
ecorpcompanygrouptuneziki <tunezikiofficial@gmail.com>
2024-01-17 12:49:13 +0300

    TUNE ZIKI OFFICALLY LAUNCHED

--- Advance-Native/START/AppStart.js
+++ Advance-Native/START/AppStart.js
@@ -4,8 +4,6 @@ const APPSTART=(DIV,ADVANCE)=>{
 
 if (localStorage.getItem('Device')==='Android') {
     
-    ADVANCE.NOTIFICATIONS('Are they Working')
-
     ANDROIDLOGINPAGE(DIV,ADVANCE)
 
 } else {
