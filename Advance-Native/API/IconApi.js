a2c7da632eac850a21b1c2a3a6f677a722b8b5a2
ecorpcompanygrouptuneziki <tunezikiofficial@gmail.com>
2024-01-17 12:54:47 +0300

    ANDROID ICONS WORKED ON

--- Advance-Native/API/IconApi.js
+++ Advance-Native/API/IconApi.js
@@ -1,6 +1,6 @@
-const WHITEICONS=`../ASSETS/ICONS/WHITE/`;
-const BLACKICONS=`../ASSETS/ICONS/BLACK/`;
-const TEALICONS=`../ASSETS/ICONS/TEAL/`;
+const WHITEICONS=`https://ecorpcompanygrouptuneziki.github.io/TuneZikiApp/Advance-Native/ASSETS/WHITE/`;
+const BLACKICONS=`https://ecorpcompanygrouptuneziki.github.io/TuneZikiApp/Advance-Native/ASSETS/BLACK/`;
+const TEALICONS=`https://ecorpcompanygrouptuneziki.github.io/TuneZikiApp/Advance-Native/ASSETS/TEAL/`;
 
 
 export{WHITEICONS,BLACKICONS,TEALICONS};
