d0a62cddeac1ed79368ccf23f76271158bd3adff
ecorpcompanygrouptuneziki <tunezikiofficial@gmail.com>
2024-01-20 02:44:16 +0300

    Tune Ziki Icons Worked On

--- Advance-Native/APIS/AccessApis.js
+++ Advance-Native/APIS/AccessApis.js
@@ -1,10 +1,10 @@
-const WHITEICONS=`../ASSETS/ICONS/WHITE/`;
-const BLACKICONS=`../ASSETS/ICONS/BLACK`;
-const TEALICONS=`../ASSETS/ICONS/TEAL/`;
-const VIDEOSAPI=`../ASSETS/VIDEOS/`;
-const SOUNDSAPI=`../ASSETS/SOUNDS/`;
-const IMAGESAPI=`../ASSETS/IMAGES/`;
-const COUNTRIESAPI=`../DATABASE/Countries.json`;
+const WHITEICONS=`https://ecorpcompanygrouptuneziki.github.io/TuneZikiApp/Advance-Native/ASSETS/ICONS/WHITE/`;
+const BLACKICONS=`https://ecorpcompanygrouptuneziki.github.io/TuneZikiApp/Advance-Native/ASSETS/ICONS/BLACK/`;
+const TEALICONS=`https://ecorpcompanygrouptuneziki.github.io/TuneZikiApp/Advance-Native/ASSETS/ICONS/TEAL/`;
+const VIDEOSAPI=`https://ecorpcompanygrouptuneziki.github.io/TuneZikiApp/Advance-Native/ASSETS/VIDEOS/`;
+const SOUNDSAPI=`https://ecorpcompanygrouptuneziki.github.io/TuneZikiApp/Advance-Native/ASSETS/SOUNDS/`;
+const IMAGESAPI=`https://ecorpcompanygrouptuneziki.github.io/TuneZikiApp/Advance-Native/ASSETS/IMAGES/`;
+const COUNTRIESAPI=`https://ecorpcompanygrouptuneziki.github.io/TuneZikiApp/Advance-Native/DATABASE/Countries.json`;
 
 
 export{WHITEICONS,BLACKICONS,TEALICONS,VIDEOSAPI,SOUNDSAPI,IMAGESAPI,COUNTRIESAPI}
